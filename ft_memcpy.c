8b9b0d50ac28a360fb54596b44bcd1b2ea43cafc
Ktaka95 <ktakada950711@gmail.com>
2022-01-20 05:08:14 +0900

    tired_will_sleep

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,71 +14,110 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*void_to_unsigned_char_dst;
-	unsigned char	*void_to_unsigned_char_src;
+	unsigned char	*unsigned_char_dst;
+	unsigned char	*unsigned_char_src;
 
-	i = 0;
-	void_to_unsigned_char_dst = (unsigned char *)dst;
-	void_to_unsigned_char_src = (unsigned char *)src;
-	while (i < n)
+	unsigned_char_dst = (unsigned char *)dst;
+	unsigned_char_src = (unsigned char *)src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	else
 	{
-		void_to_unsigned_char_dst[i] = void_to_unsigned_char_src[i];
-		i++;
+		while (n > 0)
+		{
+			unsigned_char_dst[n - 1] = unsigned_char_src[n - 1];
+			n--;
+		}
 	}
 	return (dst);
 }
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int	main()
-{
-		printf("Original_memcpy\n");
-		char    src[10];
-        char    dst[10];
-        int     i1;
+int main(void) {
+	char buffer11[10];	/* 重複しない領域用1 */
+	char buffer22[10];	/* 重複しない領域用2 */
+	char buffer33[20];	/* 重複する領域用 */
+	char buffer1[10];	/* 重複しない領域用1 */
+	char buffer2[10];	/* 重複しない領域用2 */
+	char buffer3[20];	/* 重複する領域用 */
 
-        for (i1 = 0; i1 < 10; i1++)
-        {
-                src[i1] = i1;
-        }
+	/* 重複しない領域へのcopy */
+	char * restrict s11 = buffer11;
+	char * restrict s22 = buffer22;
+	char * restrict s1 = buffer1;
+	char * restrict s2 = buffer2;
+	memset(s22, '2', sizeof(buffer22));
+	memset(s2, '2', sizeof(buffer2));
 
-        printf("1.");
-        for (i1 = 0; i1 < 10; i1++)
-        {
-                printf("[%d]", src[i1]);
-        }
-        printf("\n2.");
+	/* s22 -> s11へ5バイト分コピー */
+	memset(s11, '1', sizeof(buffer11));
+	memset(s1, '1', sizeof(buffer1));
+	fprintf(stdout, "before memcpy s11[%.10s]\n", s11);
+	fprintf(stdout, "before memcpy s22[%.10s]\n", s22);
+	fprintf(stdout, ">after memcpy s11[%.10s]\n", (char*)memcpy(s11, s22, 5));
+	fprintf(stdout, "before ft_memcpy s1[%.10s]\n", s1);
+	fprintf(stdout, "before ft_memcpy s2[%.10s]\n", s2);
+	fprintf(stdout, ">after ft_memcpy s1[%.10s]\n", (char*)ft_memcpy(s1, s2, 5));
 
-       memcpy(dst, src, 10);
+	memset(s11, '1', sizeof(buffer11));
+	memset(s1, '1', sizeof(buffer1));
 
-        for (i1 = 0; i1 < 10; i1++)
-        {
-                printf("[%d]", dst[i1]);
-        }
-		printf("\n");
-		printf("ft_memcpy\n");
-		char    src2[10];
-        char    dst2[10];
-        int     i2;
+	/* 重複する領域へのcopy その１ */
+	s11 = &buffer33[0];
+	s22 = &buffer33[1];
+	s1 = &buffer3[0];
+	s2 = &buffer3[1];
 
-        for (i2 = 0; i2 < 10; i2++)
-        {
-                src2[i2] = i2;
-        }
+	fprintf(stdout, "\n\n");
 
-        printf("1.");
-        for (i2 = 0; i2 < 10; i2++)
-        {
-                printf("[%d]", src2[i2]);
-        }
-        printf("\n2.");
+	/* s22 -> s11へ15バイト分コピー */
+	memset(&buffer33[0], '1', 10);
+	memset(&buffer33[10], '2', 10);
+	memset(&buffer3[0], '1', 10);
+	memset(&buffer3[10], '2', 10);
+	fprintf(stdout, "before memcpy buffer33[%.20s]\n", buffer33);
+	fprintf(stdout, "before memcpy s11[%.15s]\n", s11);
+	fprintf(stdout, "before memcpy s22[%.15s]\n", s22);
+	fprintf(stdout, ">after memcpy s11[%.15s]\n", (char*)memcpy(s11, s22, 15));
+	fprintf(stdout, "before ft_memcpy buffer3[%.20s]\n", buffer3);
+	fprintf(stdout, "before ft_memcpy s1[%.15s]\n", s1);
+	fprintf(stdout, "before ft_memcpy s2[%.15s]\n", s2);
+	fprintf(stdout, ">after ft_memcpy s1[%.15s]\n", (char*)ft_memcpy(s1, s2, 15));
 
-       ft_memcpy(dst2, src2, 10);
+	memset(&buffer33[0], '1', 10);
+	memset(&buffer33[10], '2', 10);
+	memset(&buffer3[0], '1', 10);
+	memset(&buffer3[10], '2', 10);
 
-        for (i2 = 0; i2 < 10; i2++)
-        {
-                printf("[%d]", dst2[i2]);
-        }
+	/* 重複する領域へのcopy その２ */
+	s11 = &buffer33[1];
+	s22 = &buffer33[0];
+	s1 = &buffer3[1];
+	s2 = &buffer3[0];
+
+	fprintf(stdout, "\n\n");
+
+	/* s22 -> s11へ15バイト分コピー */
+	memset(&buffer33[0], '1', 10);
+	memset(&buffer33[10], '2', 10);
+	memset(&buffer3[0], '1', 10);
+	memset(&buffer3[10], '2', 10);
+	fprintf(stdout, "before memcpy buffer33[%.20s]\n", buffer33);
+	fprintf(stdout, "before memcpy s11[%.15s]\n", s11);
+	fprintf(stdout, "before memcpy s22[%.15s]\n", s22);
+	fprintf(stdout, ">after memcpy s11[%.15s]\n", (char*)memcpy(s11, s22, 15));
+	fprintf(stdout, "before ft_memcpy buffer3[%.20s]\n", buffer3);
+	fprintf(stdout, "before ft_memcpy s1[%.15s]\n", s1);
+	fprintf(stdout, "before ft_memcpy s2[%.15s]\n", s2);
+	fprintf(stdout, ">after ft_memcpy s1[%.15s]\n", (char*)ft_memcpy(s1, s2, 15));
+
+	memset(&buffer33[0], '1', 10);
+	memset(&buffer33[10], '2', 10);
+	memset(&buffer3[0], '1', 10);
+	memset(&buffer3[10], '2', 10);
+
+	return(0);
 }
