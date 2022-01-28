57d2b530ceb7ae152fd40f266f8ddc9c3caebe2a
Ktaka95 <ktakada950711@gmail.com>
2022-01-29 02:35:06 +0900

    strmapi_complete

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*ret_s;
+
+	i = 0;
+	if (s == NULL)
+		return ("!!!s = NULL!!!");
+	ret_s = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (ret_s == NULL)
+		return (NULL);
+	else
+	{
+		while (s[i] != '\0')
+		{
+			ret_s[i] = f(i, s[i]);
+			i++;
+		}
+	}
+	return (ret_s);
+}
+
+// char	test_toupper(unsigned int i, char c)
+// {
+// 	i = 32;
+// 	if ('a' <= c && c <= 'z')
+// 		c -= i;
+// 	return (c);
+// }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(void)
+// {
+// 	char	*s1_basic = "abcdefg";
+// 	char	*s1_null = NULL;
+// 	char	(* testtoupper)(unsigned int, char);
+// 	char	*s2_basic;
+// 	char	*s2_null;
+// 	size_t	i = 0;
+
+// 	testtoupper = test_toupper;
+// 	s2_basic = ft_strmapi(s1_basic, testtoupper);
+// 	while (s2_basic[i] != '\0')
+// 	{
+// 		printf("%zu: [%c]\n", i, s2_basic[i]);
+// 		i++;
+// 	}
+// 	if (s2_basic[i] == '\0')
+// 		printf("%zu: [\\'0']\n", i);
+// 	s2_null = ft_strmapi(s1_null, testtoupper);
+// 	printf("%s\n", s2_null);
+// 	return (0);
+// }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,115 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include "libft.h"
 
+// // static char	*ft_chartrim(char const *s1, char set);
+
+// // static size_t	ft_topcnt(char const *s1, char const *set);
+
+// // static size_t	ft_endcnt(char const *s1, char const *set);
+
+// // char	*ft_strtrim(char const *s1, char const *set)
+// // {
+// // 	size_t	i;
+// // 	char	*trimed_str;
+
+// // 	i = 0;
+// // 	while (set[i] != '\0')
+// // 	{
+// // 		s1 = ft_chartrim(s1, set[i]);
+// // 		i++;
+// // 	}
+// // 	trimed_str = ft_strdup(s1);
+// // 	return (trimed_str);
+// // }
+
+// // char	*ft_chartrim(char const *s1, char set)
+// // {
+// // 	size_t	i;
+// // 	size_t	j;
+// // 	size_t	cnt;
+// // 	char	*heap;
+
+// // 	i = 0;
+// // 	j = 0;
+// // 	cnt = ft_allocsizecnt(s1, set);
+// // 	heap = ft_calloc(ft_strlen(s1) - cnt + 1, sizeof(char));
+// // 	while (s1[i] != '\0')
+// // 	{
+// // 		if (s1[i] != set)
+// // 		{
+// // 			heap[j] = s1[i];
+// // 			i++;
+// // 			j++;
+// // 		}
+// // 		else if (s1[i] == set)
+// // 			i++;
+// // 	}
+// // 	return (heap);
+// // }
+
+// static size_t	ft_topcnt(char const *s1, char const *set)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	cnt;
+
+// 	i = 0;
+// 	j = 0;
+// 	cnt = 0;
+// 	while (set[i] != '\0')
+// 	{
+// 		while (s1[j] == set[i])
+// 		{
+// 			j++;
+// 			cnt++;
+// 		}
+// 		i++;
+// 		j = 0;
+// 	}
+// 	return (cnt);
+// }
+
+// // static size_t	ft_endcnt(char const *s1, char const *set)
+// // {
+
+// // }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	// char	s1[] = "ABCDECFCFGF";
+// 	// char	*s2 = NULL;
+// 	// size_t	i;
+// 	size_t	j = 0;
+// 	size_t	k = 0;
+
+// 	// s2 = ft_strtrim(s1, "CF");
+// 	// i = 0;
+// 	// while (s2[i] != '\0')
+// 	// {
+// 	// 	if (s2[i] != '\0')
+// 	// 		printf("%zu: [%c]\n", i, s2[i]);
+// 	// 	else if (s2[i] == '\0')
+// 	// 		printf("%zu: ['\\0']\n", i);
+// 	// 	i++;
+// 	// }
+// 	// if (s2[i] != '\0')
+// 	// 	printf("%zu: [%c]\n", i, s2[i]);
+// 	// else if (s2[i] == '\0')
+// 	// 	printf("%zu: ['\\0']\n", i);
+// 	char	*s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
+// 	char	*s2 = "Hello \t  Please\n Trim me !";
+
+// 	// char	*ret1 = ft_strtrim(s1, " \n\t");
+// 	// char	*ret2 = ft_strtrim(s2, " \n\t");
+// 	// printf("%s\n", ret1);
+// 	// printf("-----------------\n");
+// 	// printf("%s\n", ret2);
+// 	j = ft_topcnt(s1, " \n\t");
+// 	k = ft_topcnt(s2, " \n\t");
+// 	printf("%zu, %zu\n", j, k);
+// 	return (0);
+// }
