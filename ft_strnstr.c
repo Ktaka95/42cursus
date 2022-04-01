97095cd3cb22ba7bec08277710872fff7363d6a5
Ktaka95 <ktakada950711@gmail.com>
2022-04-02 02:54:59 +0900

    strnstr_norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,22 +12,30 @@
 
 #include "libft.h"
 
+static char	*ft_strnstr_body(char *h, char *n, size_t len);
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
 	char	*h;
 	char	*n;
 
-	i = 0;
 	h = (char *)haystack;
 	n = (char *)needle;
 	if (ft_strlen(needle) == 0 || (len == 0 && haystack == NULL))
 		return (h);
 	else if (ft_strlen(needle) > ft_strlen(haystack) || len < ft_strlen(needle))
 		return (NULL);
-	while (i + ft_strlen(needle) - 1 < len)
+	return (ft_strnstr_body(h, n, len));
+}
+
+char	*ft_strnstr_body(char *h, char *n, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	while (i + ft_strlen(n) - 1 < len)
 	{
 		j = i;
 		k = 0;
@@ -48,19 +56,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (NULL);
 }
 
-#include <string.h>
-#include <stdio.h>
+// #include <string.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	char *str = NULL;
-	char *p = NULL;
-	char *q = NULL;
-	char *str2 = "abc";
-	printf("str, p, qのアドレス→%p, %p, %p\n", str, p, q);
-	p = strnstr(str, str2, 1);
-	printf("strnstr→%s: %s\n%p, %p\n", str, p, str, p);
-	q = ft_strnstr(str, str2, 1);
-	printf("ft_strnstr→%s: %s\n%p, %p\n", str, q, str, q);
-	return 0;
-}
+// int main(void)
+// {
+// 	char *str = NULL;
+// 	char *p = NULL;
+// 	char *q = NULL;
+// 	char *str2 = "abc";
+// 	printf("str, p, qのアドレス→%p, %p, %p\n", str, p, q);
+// 	p = strnstr(str, str2, 1);
+// 	printf("strnstr→%s: %s\n%p, %p\n", str, p, str, p);
+// 	q = ft_strnstr(str, str2, 1);
+// 	printf("ft_strnstr→%s: %s\n%p, %p\n", str, q, str, q);
+// 	return 0;
+// }
