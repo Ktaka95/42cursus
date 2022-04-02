8d2d5796729278efcfe4f5d383b549c2a01e3f83
Ktaka95 <ktakada950711@gmail.com>
2022-04-02 13:14:33 +0900

    strchr_null_case_check

--- ft_strchr.c
+++ ft_strchr.c
@@ -40,25 +40,13 @@ char	*ft_strchr(const char *s, int c)
 
 // int main(void)
 // {
-// 	char	str[] = "abcdefghijklmnopqrstuvdwxyz", *p1, *p2;
-// 	int		c = 'd';
-// 	int		i = 0;
+// 	char	str[] = "abcdefghijkl\0mnopqrstuvdwxyz", *p1, *p2;
+// 	int		c = '\0';
 
-// 	while (i < INT_MAX)
-// 	{
-// 		p1 = strchr(str, c + i);
-// 		p2 = ft_strchr(str, c + i);
-// 		if (p1 == NULL && p2 == NULL)
-// 			printf("NULL, %d\n", i);
-// 		else
-// 			if (strcmp(p1, p2) == 0)
-// 				printf("OK, %d\n", i);
-// 			else
-// 			{
-// 				printf("%d, NG :(\n", i);
-// 				exit (0);
-// 			}
-// 		i += 128;
-// 	}
+// 	p1 = strchr(str, c);
+// 	p2 = ft_strchr(str, c);
+// 	p1++;
+// 	p2++;
+// 	printf("%s, %s\n", p1, p2);
 // 	return 0;
 // }
