d0bedb0422bc0d95ff5867bf81a5ade1c02f2c79
Ktaka95 <ktakada950711@gmail.com>
2022-04-02 17:05:37 +0900

    tolower_test_comp

--- ft_tolower.c
+++ ft_tolower.c
@@ -19,30 +19,25 @@ int	ft_tolower(int c)
 	return (c);
 }
 
-// #include <ctype.h>
-// #include <stdio.h>
+/*
+#include <stdio.h>
+#include <ctype.h>
 
-// int main(void)
-// {
-// 	int c1;
-// 	int c11;
-// 	c1 = tolower('A');
-// 	c11 = ft_tolower('A');
-// 	printf("Aを小文字に変換→%c\n",c1);
-// 	printf("Aを小文字に変換→%c\n",c1);
+int	main(void)
+{
+	int	c = -128;
 
-// 	int c2;
-// 	int c22;
-// 	c2 = tolower('a');
-// 	c22 = ft_tolower('a');
-// 	printf("aを小文字に変換→%c\n",c2);
-// 	printf("aを小文字に変換→%c\n",c22);
-
-// 	int c3;
-// 	int c33;
-// 	c3 = tolower('1');
-// 	c33 = ft_tolower('1');
-// 	printf("1を小文字に変換→%c\n",c3);
-// 	printf("1を小文字に変換→%c\n",c33);
-// 	return 0;
-// }
+	while (c < 257)
+	{
+		if (tolower(c) != ft_tolower(c))
+		{
+			printf("NG :(\n");
+			exit (0);
+		}
+		else
+			printf("ascii %d: %c =>\tOK\n", c, (char)c);
+		c++;
+	}
+	printf("OK :)\n");
+}
+*/
