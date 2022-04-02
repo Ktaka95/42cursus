b8c2c36e482789f1bf04165efb54190e824b7d9b
Ktaka95 <ktakada950711@gmail.com>
2022-04-02 13:19:32 +0900

    strrchr_middle_nullchar_check

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -38,25 +38,13 @@ char	*ft_strrchr(const char *s, int c)
 
 // int main(void)
 // {
-// 	char	str[] = "abcdefghijklmnopqrstuvdwxyz", *p1, *p2;
-// 	int		c = 'd';
-// 	int		i = 0;
+// 	char	str[] = "abc\0de", *p1, *p2;
+// 	int		c = '\0';
 
-// 	while (i < INT_MAX)
-// 	{
-// 		p1 = strrchr(str, c + i);
-// 		p2 = ft_strrchr(str, c + i);
-// 		if (p1 == NULL && p2 == NULL)
-// 			printf("NULL, %d\n", i);
-// 		else
-// 			if (strcmp(p1, p2) == 0)
-// 				printf("OK, %d\n", i);
-// 			else
-// 			{
-// 				printf("%d, NG :(\n", i);
-// 				exit (0);
-// 			}
-// 		i += 128;
-// 	}
+// 	p1 = strrchr(str, c);
+// 	p2 = ft_strrchr(str, c);
+// 	p1--;
+// 	p2--;
+// 	printf("%s, %s\n", p1, p2);
 // 	return 0;
 // }
