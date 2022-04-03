aa62766c3d2b67250d19927af3dd44f5b08b61d3
Ktaka95 <ktakada950711@gmail.com>
2022-04-03 16:42:47 +0900

    memset_test_comp

--- ft_memset.c
+++ ft_memset.c
@@ -15,154 +15,169 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
-	unsigned char	*unsigned_char_b;
+	unsigned char	*str;
 
 	i = 0;
-	unsigned_char_b = (unsigned char *)b;
+	str = (unsigned char *)b;
 	while (i < len)
 	{
-		unsigned_char_b[i] = c;
+		str[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
 }
 
-// #include <string.h>
-// #include <stdio.h>
+/*
+#include <stdio.h>
+#include <string.h>
 
-// int	main(void)
-// {
-// 	int	str1[] = {1, 2, 3, 4, 5};
-// 	char	str2[] = "AB\0CD\0EF\0G";
-// 	int	*p;
-// 	char	*q;
-// 	int	i;
-// 	p = str1;
-// 	q = str2;
-// 	i = 0;
-// 	printf("Before_memset\n");
-// 	while (i < 5)
-// 	{
-// 		printf("%d", str1[i]);
-// 		i++;
-// 		p++;
-// 	}
-// 	printf("\n");
-// 	i = 0;
-// 	while (i < 10)
-// 	{
-// 		printf("[%c]", str2[i]);
-// 		i++;
-// 		q++;
-// 	}
-// 	printf("\n");
-// 	printf("After_memset\n");
-// 	p = str1;
-// 	i = 0;
-// 	memset(p, 0, 3);
-// 	while (i < 5)
-// 	{
-// 		printf("%d", str1[i]);
-// 		i++;
-// 		p++;
-// 	}
-// 	printf("\n");
-// 	q = str2;
-// 	i = 0;
-// 	memset(q, 0, 5);
-// 	while (i < 10)
-// 	{
-// 		printf("[%c]", str2[i]);
-// 		i++;
-// 		p++;
-// 	}
-// 	printf("\n");
-// 	printf("---------------\n");
-// 	int	str3[] = {1, 2, 3, 4, 5};
-// 	char	str4[] = "AB\0CD\0EF\0G";
-// 	int	*r;
-// 	char	*s;
-// 	r = str3;
-// 	s = str4;
-// 	i = 0;
-// 	printf("Before_ft_memset\n");
-// 	while (i < 5)
-// 	{
-// 		printf("%d", str3[i]);
-// 		i++;
-// 		r++;
-// 	}
-// 	printf("\n");
-// 	i = 0;
-// 	while (i < 10)
-// 	{
-// 		printf("[%c]", str4[i]);
-// 		i++;
-// 		s++;
-// 	}
-// 	printf("\n");
-// 	printf("After_memset\n");
-// 	r = str3;
-// 	i = 0;
-// 	ft_memset(r, 0, 3);
-// 	while (i < 5)
-// 	{
-// 		printf("%d", str3[i]);
-// 		i++;
-// 		r++;
-// 	}
-// 	printf("\n");
-// 	s = str4;
-// 	i = 0;
-// 	ft_memset(s, 0, 5);
-// 	while (i < 10)
-// 	{
-// 		printf("[%c]", str4[i]);
-// 		i++;
-// 		s++;
-// 	}
-// 	printf("\n");
-// 	return (0);
-// }
+void	test1_normal_case(void);
+void	test2_len0_case(void);
+void	test3_b_empty_len0_case(void);
+void	test4_b_NULL_len0_case(void);
+void	test5_len_gt_b_case(void);
+void	test6_b_NULL_case(void);
 
-// #include <stdio.h>
-// #include <string.h>
-// #include <limits.h>
+int	main(void)
+{
+	test1_normal_case();
+	test2_len0_case();
+	test3_b_empty_len0_case();
+	test4_b_NULL_len0_case();
+	test5_len_gt_b_case();
+	test6_b_NULL_case();
+	return (0);
+}
 
-// int main(void)
-// {
-// 	size_t n = INT_MAX;
-// 	n += 5;
-// 	size_t	i;
+void	test1_normal_case(void)
+{
+	char	ori_arry[] = "Hello, world!";
+	char	ft_arry[] = "Hello, world!";
+	int		c = '?';
+	size_t	len = 5;
 
-// 	i = 0;
-	// char *str = malloc(sizeof(char) * n);
-	// if (str == NULL)
-	// 	printf("Err\n");
-	// else
-	// {
-	// 	ft_memset(str, 'a', n);
-	// 	while (i < n)
-	// 	{
-	// 		printf("%c", str[i]);
-	// 		i++;
-	// 	}
-	// }
-	// free(str);
+	printf("///test1_normal_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	if (strcmp(ori_arry, ft_arry) == 0)
+		printf("after ori_memset =>\t%s\n\
+after ft_memset =>\t%s\nOK :)\n\n",ori_arry, ft_arry);
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
 
-// 	int *arry = malloc(sizeof(int) * n);
+void	test2_len0_case(void)
+{
+	char	ori_arry[] = "Hello, world!";
+	char	ft_arry[] = "Hello, world!";
+	int		c = '?';
+	size_t	len = 0;
 
-// 	if (arry == NULL)
-// 		printf("Err\n");
-// 	else
-// 	{
-// 		memset(arry, 0, n);
-// 		while (i < n)
-// 		{
-// 			printf("%d", arry[i]);
-// 			i++;
-// 		}
-// 	}
-// 	free(arry);
-// 	return (0);
-// }
+	printf("///test2_len0_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	if (strcmp(ori_arry, ft_arry) == 0)
+		printf("after ori_memset =>\t%s\n\
+after ft_memset =>\t%s\nOK :)\n\n",ori_arry, ft_arry);
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test3_b_empty_len0_case(void)
+{
+	char	ori_arry[] = "";
+	char	ft_arry[] = "";
+	int		c = '?';
+	size_t	len = 0;
+
+	printf("///test3_b_empty_len0_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	if (strcmp(ori_arry, ft_arry) == 0)
+		printf("after ori_memset =>\t%s\n\
+after ft_memset =>\t%s\nOK :)\n\n",ori_arry, ft_arry);
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test4_b_NULL_len0_case(void)
+{
+	char	*ori_arry = NULL;
+	char	*ft_arry = NULL;
+	int		c = '?';
+	size_t	len = 0;
+
+	printf("///test4_b_NULL_len0_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	if (ori_arry == NULL && ft_arry == NULL)
+		printf("after ori_memset =>\t%s\n\
+after ft_memset =>\t%s\nOK :)\n\n",ori_arry, ft_arry);
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test5_len_gt_b_case(void)
+{
+	char	ori_arry[] = "Hello, world!";
+	char	ft_arry[] = "Hello, world!";
+	int		c = '?';
+	size_t	len = 100;
+
+	printf("///test5_len_gt_b_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	printf("test5_following_case_abort\n");
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	if (strcmp(ori_arry, ft_arry) == 0)
+		printf("after ori_memset =>\t%s\n\
+after ft_memset =>\t%s\nOK :)\n\n",ori_arry, ft_arry);
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test6_b_NULL_case(void)
+{
+	char	*ori_arry = NULL;
+	char	*ft_arry = NULL;
+	int		c = '?';
+	size_t	len = 10;
+
+	printf("///test6_b_NULL_case///\n\
+b: %s\nc: \'%c\'\nlen: %zu\n", ori_arry, c, len);
+	printf("before memset: arry =>\t%s\n", ori_arry);
+	printf("test6_following_case_segv\n");
+	memset(ori_arry, c, len);
+	ft_memset(ft_arry, c, len);
+	return ;
+}
+*/
