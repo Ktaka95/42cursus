e23cb5b2262f1091a721f4c322863ed26327070e
Ktaka95 <ktakada950711@gmail.com>
2022-04-04 11:18:45 +0900

    putnbr_ft_test_comp

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -36,3 +36,73 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(tmp % 10, fd);
 	}
 }
+
+/*
+#include <stdio.h>
+#include <unistd.h>
+
+void	test1_INT_MIN_to_0_case(void);
+void	test2_0_to_INT_MAX_case(void);
+
+int	main(void)
+{
+	test1_INT_MIN_to_0_case();
+	sleep(10);
+	test2_0_to_INT_MAX_case();
+	return (0);
+}
+
+void	test1_INT_MIN_to_0_case(void)
+{
+	int	n = INT_MIN;
+	int	fd = 1;
+
+	while (n < 0)
+	{
+		while (fd < 3)
+		{
+			ft_putnbr_fd(n, fd);
+			printf("\n");
+			fd++;
+		}
+		fd = 1;
+		n += 8192;
+		if (n == 0)
+		{
+			while (fd < 3)
+			{
+				ft_putnbr_fd(n, fd);
+				printf("\n");
+				fd++;
+			}
+		}
+	}
+}
+
+void	test2_0_to_INT_MAX_case(void)
+{
+	int	n = 1;
+	int	fd = 1;
+
+	while (n < INT_MAX)
+	{
+		while (fd < 3)
+		{
+			ft_putnbr_fd(n, fd);
+			printf("\n");
+			fd++;
+		}
+		fd = 1;
+		n += 11627;
+		if (n == INT_MAX)
+		{
+			while (fd < 3)
+			{
+				ft_putnbr_fd(n, fd);
+				printf("\n");
+				fd++;
+			}
+		}
+	}
+}
+*/
