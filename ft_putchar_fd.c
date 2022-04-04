58d53a9bc6e0582c51b9c5a19d01336b4af5eb6e
Ktaka95 <ktakada950711@gmail.com>
2022-04-04 10:53:51 +0900

    putchar_fd_test_comp

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -16,3 +16,20 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int		fd = 1;
+	char	c = 'A';
+
+	while (fd < 3)
+	{
+		ft_putchar_fd(c, fd);
+		printf("\n");
+		fd++;
+	}
+	return (0);
+}
+*/
