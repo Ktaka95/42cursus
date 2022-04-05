1fc40b67d48233da2a5941f514f91d9ade7339bc
Ktaka95 <ktakada950711@gmail.com>
2022-04-05 17:28:36 +0900

    substr_test_comp

--- ft_substr.c
+++ ft_substr.c
@@ -39,25 +39,98 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 }
 
-// #include <stdio.h>
-// #include <string.h>
+/*
+#include <stdio.h>
+#include <string.h>
 
-// int main(void)
-// {
-// 	// char	*main_s = "abcdef";
-// 	// char	*sub_s = NULL;
-// 	// int		i = 0;
-// 	// sub_s = ft_substr(main_s, 0, 5);
-// 	// while (sub_s[i] != '\0')
-// 	// {
-// 	// 	printf("%d: [%c]\n", i, sub_s[i]);
-// 	// 	i++;
-// 	// }
-// 	// printf("%d: [%c]\n", i, sub_s[i]);
-// 	// printf("%s\n", sub_s);
-// 	char * ft_s = ft_substr("tripouille", 0, 42000);
-// 	printf("%s\n", ft_s);
-// 	char * mcheck = malloc(strlen("tripouille" + 1));
-// 	printf("%s\n", mcheck);
-// 	return (0);
-// }
+void	test1_normal_case(void);
+void	test2_s_null_case(void);
+void	test3_len_0_case(void);
+void	test4_start_longer_than_s_len_case(void);
+
+int	main(void)
+{
+	test1_normal_case();
+	test2_s_null_case();
+	test3_len_0_case();
+	test4_start_longer_than_s_len_case();
+	return (0);
+}
+
+void	test1_normal_case(void)
+{
+	char	*str = "Welcome!! Hello, 42 world!";
+	char	*target = "Hello, 42 world!";
+	size_t	start = 10;
+	size_t	len = strlen(target);
+	char	*ret = NULL;
+
+	printf("///test1_normal_case///\n");
+	printf("--before substr--\n");
+	printf("str: %s\nstart: %zu\nlen: %zu\n", str, start, len);
+	ret = ft_substr(str, 10, strlen(target));
+	if (strcmp(target, ret) == 0)
+	{
+		printf("--after substr--\n");
+		printf("%s\n", ret);
+		printf("OK :)\n");
+	}
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test2_s_null_case(void)
+{
+	char	*str = NULL;
+	size_t	start = 10;
+	size_t	len = 10;
+	char	*ret = NULL;
+
+	printf("\n///test2_s_null_case///\n");
+	printf("--before substr--\n");
+	printf("str: %s\nstart: %zu\nlen: %zu\n", str, start, len);
+	ret = ft_substr(str, start, len);
+	printf("--after substr--\n");
+	if (strlen(ret) == 0)
+		printf("EMPTY\n");
+	return ;
+}
+
+void	test3_len_0_case(void)
+{
+	char	*str = "Hello, 42 world!";
+	size_t	start = 10;
+	size_t	len = 0;
+	char	*ret = NULL;
+
+	printf("\n///test3_len_0_case///\n");
+	printf("--before substr--\n");
+	printf("str: %s\nstart: %zu\nlen: %zu\n", str, start, len);
+	ret = ft_substr(str, start, len);
+	printf("--after substr--\n");
+	if (strlen(ret) == 0)
+		printf("EMPTY\n");
+	return ;
+}
+
+void	test4_start_longer_than_s_len_case(void)
+{
+	char	*str = "Hello, 42 world!";
+	size_t	start = strlen(str) + 1;
+	size_t	len = 10;
+	char	*ret = NULL;
+
+	printf("\n///test4_start_longer_than_s_len_case///\n");
+	printf("--before substr--\n");
+	printf("str: %s\nstart: %zu\nlen: %zu\n", str, start, len);
+	ret = ft_substr(str, start, len);
+	printf("--after substr--\n");
+	if (strlen(ret) == 0)
+		printf("EMPTY\n");
+	return ;
+}
+*/
