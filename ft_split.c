ec82d9bf69d994a54974c76b93ed6032b91f528e
Ktaka95 <ktakada950711@gmail.com>
2022-04-05 21:26:51 +0900

    split_back_before_ver

--- ft_split.c
+++ ft_split.c
@@ -14,8 +14,6 @@
 
 static size_t	ft_split_count(char const *s, char c);
 
-static void		*ft_split_free(char **ret, size_t n);
-
 char	**ft_split(char const *s, char c)
 {
 	char			**ret;
@@ -36,12 +34,7 @@ char	**ft_split(char const *s, char c)
 		while (s[i] != c && s[i] != '\0')
 			i++;
 		if (i > start)
-		{
-			ret[j] = ft_substr(s, start, i - start);
-			if (ret[j] == NULL)
-				return (ft_split_free(ret, j));
-			j++;
-		}
+			ret[j++] = ft_substr(s, start, i - start);
 	}
 	ret[j] = NULL;
 	return (ret);
@@ -73,23 +66,8 @@ size_t	ft_split_count(char const *s, char c)
 	return (count);
 }
 
-void	*ft_split_free(char **ret, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		free(ret[i]);
-		i++;
-	}
-	free(ret);
-	return (NULL);
-}
-
 /*
 #include <stdio.h>
-
 void	test1_normal_case(void);
 void	test2_nosplit_case(void);
 void	test3_c_0_case(void);
@@ -101,7 +79,6 @@ void	test8_s_empty_case(void);
 void	test9_s_empty_c_null_case(void);
 void	test10_s_null_case(void);
 void	test11_s_null_c_0_case(void);
-
 int	main(void)
 {
 	test1_normal_case();
@@ -117,14 +94,12 @@ int	main(void)
 	test11_s_null_c_0_case();
 	return (0);
 }
-
 void	test1_normal_case(void)
 {
 	char	**ret = NULL;
 	char	*str = " Hello, 42 world ! ";
 	char	c = ' ';
 	size_t	i = 0;
-
 	printf("///test1_normal_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -134,14 +109,12 @@ void	test1_normal_case(void)
 	}
 	return ;
 }
-
 void	test2_nosplit_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "Hello, 42 world!";
 	char	c = 'A';
 	size_t	i = 0;
-
 	printf("\n///test2_nosplit_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -151,14 +124,12 @@ void	test2_nosplit_case(void)
 	}
 	return ;
 }
-
 void	test3_c_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "Hello, 42 world!";
 	char	c = '\0';
 	size_t	i = 0;
-
 	printf("\n///test3_c_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -168,14 +139,12 @@ void	test3_c_0_case(void)
 	}
 	return ;
 }
-
 void	test4_s_include_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = " Hello, \0 42 world ! ";
 	char	c = ' ';
 	size_t	i = 0;
-
 	printf("\n///test4_s_include_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -185,14 +154,12 @@ void	test4_s_include_0_case(void)
 	}
 	return ;
 }
-
 void	test5_s_include_0_c_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = " Hello, \0 42 world ! ";
 	char	c = '\0';
 	size_t	i = 0;
-
 	printf("\n///test5_s_include_0_c_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -202,14 +169,12 @@ void	test5_s_include_0_c_0_case(void)
 	}
 	return ;
 }
-
 void	test6_s_start_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "\0 Hello, 42 world ! ";
 	char	c = ' ';
 	size_t	i = 0;
-
 	printf("\n///test6_s_start_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -219,14 +184,12 @@ void	test6_s_start_0_case(void)
 	}
 	return ;
 }
-
 void	test7_s_start_0_c_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "\0 Hello, 42 world ! ";
 	char	c = '\0';
 	size_t	i = 0;
-
 	printf("\n///test7_s_start_0_c_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -236,14 +199,12 @@ void	test7_s_start_0_c_0_case(void)
 	}
 	return ;
 }
-
 void	test8_s_empty_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "";
 	char	c = 'A';
 	size_t	i = 0;
-
 	printf("\n///test8_s_empty_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -253,14 +214,12 @@ void	test8_s_empty_case(void)
 	}
 	return ;
 }
-
 void	test9_s_empty_c_null_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "";
 	char	c = '\0';
 	size_t	i = 0;
-
 	printf("\n///test9_s_empty_c_null_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -270,14 +229,12 @@ void	test9_s_empty_c_null_case(void)
 	}
 	return ;
 }
-
 void	test10_s_null_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "NULL";
 	char	c = 'A';
 	size_t	i = 0;
-
 	printf("\n///test10_s_null_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
@@ -287,14 +244,12 @@ void	test10_s_null_case(void)
 	}
 	return ;
 }
-
 void	test11_s_null_c_0_case(void)
 {
 	char	**ret = NULL;
 	char	*str = "NULL";
 	char	c = '\0';
 	size_t	i = 0;
-
 	printf("\n///test11_s_null_c_0_case///\n");
 	ret = ft_split(str, c);
 	while (ret[i] != NULL)
