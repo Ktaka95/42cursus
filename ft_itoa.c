8cf2ccfcf5236b63563b19efce63b7f370387bd7
Ktaka95 <ktakada950711@gmail.com>
2022-04-08 11:42:15 +0900

    itoa_1func_if_arg>0 || <0)

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,147 +14,113 @@
 
 static size_t	digit_check(int n);
 
-static char		*ft_itoa_if_argument_plus(int n);
-
-static char		*ft_itoa_if_argument_minus(int n);
-
 char	*ft_itoa(int n)
 {
-	if (n >= 0)
-		return (ft_itoa_if_argument_plus(n));
-	else
-		return (ft_itoa_if_argument_minus(n));
+	size_t			digit_count;
+	char			*ret;
+	unsigned int	num;
+
+	digit_count = digit_check(n);
+	ret = malloc((digit_count + 1) * sizeof(char));
+	if (ret == NULL)
+		return (NULL);
+	ret[digit_count--] = '\0';
+	num = n;
+	if (n == 0)
+		ret[0] = '0';
+	else if (n < 0)
+	{
+		num = n * -1;
+		ret[0] = '-';
+	}
+	while (digit_count >= 0 && num != 0)
+	{
+		ret[digit_count--] = num % 10 + '0';
+		num /= 10;
+	}
+	return (ret);
 }
 
 size_t	digit_check(int n)
 {
-	size_t		cnt;
+	size_t		count;
 	long int	num;
 
-	cnt = 1;
+	count = 1;
 	num = n;
 	if (num < 0)
+	{
 		num *= -1;
-	if (0 <= num && num <= 9)
-		return (cnt);
+		count++;
+	}
 	while (num >= 10)
 	{
 		num /= 10;
-		cnt++;
+		count++;
 	}
-	return (cnt);
+	return (count);
 }
 
-char	*ft_itoa_if_argument_plus(int n)
-{
-	size_t		digit_cnt;
-	size_t		i;
-	char		*ret;
+// #include <stdio.h>
+// #include <limits.h>
 
-	digit_cnt = digit_check(n);
-	i = 0;
-	ret = ft_calloc(digit_cnt + 1, sizeof(char));
-	if (ret == NULL)
-		return (NULL);
-	while (i < digit_cnt - 1)
-		i++;
-	while (i > 0)
-	{
-		ret[i] = n % 10 + '0';
-		n /= 10;
-		i--;
-	}
-	ret[i] = n + '0';
-	return (ret);
-}
+// void	test1_INT_MIN_to_0_case(void);
+// void	test2_1_to_INT_MAX_case(void);
 
-char	*ft_itoa_if_argument_minus(int n)
-{
-	size_t			digit_cnt;
-	size_t			i;
-	char			*ret;
-	unsigned int	num;
+// int main(void)
+// {
+// 	test1_INT_MIN_to_0_case();
+// 	sleep(5);
+// 	test2_1_to_INT_MAX_case();
+// 	return (0);
+// }
 
-	digit_cnt = digit_check(n);
-	i = 0;
-	ret = ft_calloc(digit_cnt + 2, sizeof(char));
-	if (ret == NULL)
-		return (NULL);
-	num = n * -1;
-	while (i < digit_cnt)
-		i++;
-	while (i > 0)
-	{
-		ret[i] = num % 10 + '0';
-		num /= 10;
-		i--;
-	}
-	ret[i] = '-';
-	return (ret);
-}
+// void	test1_INT_MIN_to_0_case(void)
+// {
+// 	int	n = INT_MIN;
+// 	char	*num = NULL;
+// 	int		check;
 
-/*
-#include <stdio.h>
-#include <limits.h>
+// 	printf("///test1_INT_MIN_to_0///\n");
+// 	sleep(2);
+// 	while (n <= 0)
+// 	{
+// 		num = ft_itoa(n);
+// 		check = atoi(num);
+// 		if (check == n)
+// 			printf("%d: OK\n", n);
+// 		else
+// 		{
+// 			printf("NG :(\n");
+// 			exit (0);
+// 		}
+// 		if (n == 0)
+// 			break ;
+// 		n += 8192;
+// 	}
+// }
 
-void	test1_INT_MIN_to_0_case(void);
-void	test2_1_to_INT_MAX_case(void);
+// void	test2_1_to_INT_MAX_case(void)
+// {
+// 	int		n = 1;
+// 	char	*num = NULL;
+// 	int		check;
 
-int main(void)
-{
-	test1_INT_MIN_to_0_case();
-	sleep(5);
-	test2_1_to_INT_MAX_case();
-	return (0);
-}
-
-void	test1_INT_MIN_to_0_case(void)
-{
-	int	n = INT_MIN;
-	char	*num = NULL;
-	int		check;
-
-	printf("///test1_INT_MIN_to_0///\n");
-	sleep(2);
-	while (n <= 0)
-	{
-		num = ft_itoa(n);
-		check = atoi(num);
-		if (check == n)
-			printf("%d: OK\n", n);
-		else
-		{
-			printf("NG :(\n");
-			exit (0);
-		}
-		if (n == 0)
-			break ;
-		n += 8192;
-	}
-}
-
-void	test2_1_to_INT_MAX_case(void)
-{
-	int		n = 1;
-	char	*num = NULL;
-	int		check;
-
-	printf("\n///test2_1_to_INT_MAX_case///\n");
-	sleep(2);
-	while (n <= INT_MAX)
-	{
-		num = ft_itoa(n);
-		check = atoi(num);
-		if (check == n)
-			printf("%d: OK\n", n);
-		else
-		{
-			printf("NG :(\n");
-			exit (0);
-		}
-		if (n == INT_MAX)
-			break ;
-		n += 11627;
-	}
-}
-*/
+// 	printf("\n///test2_1_to_INT_MAX_case///\n");
+// 	sleep(2);
+// 	while (n <= INT_MAX)
+// 	{
+// 		num = ft_itoa(n);
+// 		check = atoi(num);
+// 		if (check == n)
+// 			printf("%d: OK\n", n);
+// 		else
+// 		{
+// 			printf("NG :(\n");
+// 			exit (0);
+// 		}
+// 		if (n == INT_MAX)
+// 			break ;
+// 		n += 11627;
+// 	}
+// }
