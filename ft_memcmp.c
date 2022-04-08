39df81f9a0f61ccc72d8917d42aa60114ec2cd4d
Ktaka95 <ktakada950711@gmail.com>
2022-04-08 22:56:32 +0900

    memcmp_ret_rm

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,24 +17,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 	size_t			i;
-	int				ret;
 
-	if (n == 0)
-		return (0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	ret = 0;
 	while (i < n)
 	{
 		if (str1[i] != str2[i])
-		{
-			ret = str1[i] - str2[i];
-			break ;
-		}
+			return (str1[i] - str2[i]);
 		i++;
 	}
-	return (ret);
+	return (0);
 }
 
 /*
