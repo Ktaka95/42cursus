ab76f7229363e5453426d145e00f7d05bfe1dd8d
Ktaka95 <ktakada950711@gmail.com>
2022-04-09 14:54:44 +0900

    strlcpy_rm_else_add_test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -21,15 +21,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	ret = ft_strlen(src);
 	if (dstsize == 0)
 		return (ret);
-	else
+	while (i < dstsize - 1 && src[i] != '\0')
 	{
-		while (i < dstsize - 1 && src[i] != '\0')
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (ret);
 }
 
@@ -38,41 +35,51 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 #include <string.h>
 
 void	test1_normal_case(void);
-void	test2_dstsize_lt_src_case(void);
+void	test2_dstsize_lt_len_case(void);
 void	test3_dst_include_0_case(void);
 void	test4_src_include_0_case(void);
 void	test5_dst_null_dstsize_0_case(void);
 void	test6_dstsize_0_case(void);
 void	test7_src_null_case(void);
 void	test8_dst_null_dstsize_not_0_case(void);
+void	test9_not_null_end_case(void);
+void	test10_dst_overlap_src_case(void);
+void	test11_dst_overlap_src_case2(void);
 
 int	main(void)
 {
 	test1_normal_case();
-	test2_dstsize_lt_src_case();
+	test2_dstsize_lt_len_case();
 	test3_dst_include_0_case();
 	test4_src_include_0_case();
 	test5_dst_null_dstsize_0_case();
 	test6_dstsize_0_case();
 	test7_src_null_case();
 	test8_dst_null_dstsize_not_0_case();
+	test9_not_null_end_case();
+	test10_dst_overlap_src_case();
+	test11_dst_overlap_src_case2();
 	return (0);
 }
 
 void	test1_normal_case(void)
 {
-	char	ori_dst[30] = "override me";
-	char	ft_dst[30] = "override me";
-	char	*src = "Hello, 42 world!";
+	char	ori_dst[30] = "Hello, \0";
+	char	ft_dst[30] = "Hello, \0";
+	char	*src = "42 world!";
 	size_t	dstsize = strlen(src) + 1;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
 	printf("///test1_normal_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -83,20 +90,24 @@ void	test1_normal_case(void)
 	return ;
 }
 
-void	test2_dstsize_lt_src_case(void)
+void	test2_dstsize_lt_len_case(void)
 {
-	char	ori_dst[30] = "override me";
-	char	ft_dst[30] = "override me";
-	char	*src = "Hello, 42 world!";
-	size_t	dstsize = 9;
+	char	ori_dst[30] = "Hello, \0";
+	char	ft_dst[30] = "Hello, \0";
+	char	*src = "42 world!";
+	size_t	dstsize = strlen(src) - 1;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
-	printf("\n///test2_dstsize_lt_src_case///\n");
+	printf("\n///test2_dstsize_lt_len_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -109,18 +120,22 @@ void	test2_dstsize_lt_src_case(void)
 
 void	test3_dst_include_0_case(void)
 {
-	char	ori_dst[30] = "override \0 me";
-	char	ft_dst[30] = "override \0 me";
-	char	*src = "Hello, 42 world!";
+	char	ori_dst[30] = "Hello, \0 42 world!";
+	char	ft_dst[30] = "Hello, \0 42 world!";
+	char	*src = "42 world!";
 	size_t	dstsize = strlen(src) + 1;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
 	printf("\n///test3_dst_include_0_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -133,18 +148,22 @@ void	test3_dst_include_0_case(void)
 
 void	test4_src_include_0_case(void)
 {
-	char	ori_dst[30] = "override me";
-	char	ft_dst[30] = "override me";
-	char	*src = "Hello, 42 \0 world!";
-	size_t	dstsize = strlen(src);
+	char	ori_dst[30] = "Hello, \0";
+	char	ft_dst[30] = "Hello, \0";
+	char	*src = "42 \0 world!";
+	size_t	dstsize = strlen(src) + 1;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
 	printf("\n///test4_src_include_0_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -161,14 +180,18 @@ void	test5_dst_null_dstsize_0_case(void)
 	char	*ft_dst = NULL;
 	char	*src = "Hello, 42 world!";
 	size_t	dstsize = 0;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
 	printf("\n///test5_dst_null_dstsize_0_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -181,18 +204,22 @@ void	test5_dst_null_dstsize_0_case(void)
 
 void	test6_dstsize_0_case(void)
 {
-	char	ori_dst[30] = "override me";
-	char	ft_dst[30] = "override me";
-	char	*src = "Hello, 42 world!";
+	char	ori_dst[30] = "Hello, \0";
+	char	ft_dst[30] = "Hello, \0";
+	char	*src = "42 world!";
 	size_t	dstsize = 0;
+	size_t	ori_ret;
+	size_t	ft_ret;
 
 	printf("\n///test6_dstsize_0_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
-	if (strlcpy(ori_dst, src, dstsize) == ft_strlcpy(ft_dst, src, dstsize))
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
 	{
 		printf("--after strlcpy--\n");
-		printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 		printf("OK :)\n");
 	}
 	else
@@ -205,11 +232,10 @@ void	test6_dstsize_0_case(void)
 
 void	test7_src_null_case(void)
 {
-	char	ori_dst[30] = "override me";
-	// char	ft_dst[30] = "override me";
+	char	ori_dst[30] = "Hello, 42 world!\0";
+	// char	ft_dst[30] = "Hello, 42 world!";
 	char	*src = NULL;
 	size_t	dstsize = 0;
-
 	printf("\n///test7_src_null_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
@@ -234,9 +260,8 @@ void	test8_dst_null_dstsize_not_0_case(void)
 {
 	char	*ori_dst = NULL;
 	// char	*ft_dst = NULL;
-	char	*src = NULL;
+	char	*src = "Hello, 42 world!";
 	size_t	dstsize = 1;
-
 	printf("\n///test8_dst_null_dstsize_not_0_case///\n");
 	printf("--before strlcpy--\n");
 	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
@@ -247,6 +272,98 @@ void	test8_dst_null_dstsize_not_0_case(void)
 	// {
 	// 	printf("--after strlcpy--\n");
 	// 	printf("ret: %zu\ndst: %s\n", strlcpy(ori_dst, src, dstsize), ori_dst);
+	// 	printf("OK :)\n");
+	// }
+	// else
+	// {
+	// 	printf("NG :(\n");
+	// 	exit (0);
+	// }
+	return ;
+}
+
+void	test9_not_null_end_case(void)
+{
+	char	ori_dst[30] = "Hello, \0";
+	char	ft_dst[30] = "Hello, \0";
+	char	*src = "42 world!";
+	size_t	dstsize = strlen(src);
+	size_t	ori_ret;
+	size_t	ft_ret;
+
+	printf("\n///test9_not_null_end_case///\n");
+	printf("--before strlcpy--\n");
+	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, src, dstsize);
+	ori_ret = strlcpy(ori_dst, src, dstsize);
+	ft_ret = ft_strlcpy(ft_dst, src, dstsize);
+	if (ori_ret == ft_ret)
+	{
+		printf("--after strlcpy--\n");
+		printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
+		printf("OK :)\n");
+	}
+	else
+	{
+		printf("NG :(\n");
+		exit (0);
+	}
+	return ;
+}
+
+void	test10_dst_overlap_src_case(void)
+{
+	char	ori_dst[30] = "Hello, 42 world!\0";
+	// char	ft_dst[30] = "Hello, 42 world!\0";
+	char	*ori_src;
+	// char	*ft_src;
+	size_t	dstsize = strlen(ori_dst) * 2;
+	// size_t	ori_ret;
+	// size_t	ft_ret;
+
+	ori_src = &ori_dst[7];
+	// ft_src = &ft_dst[7];
+	printf("\n///test10_dst_overlap_src_case///\n");
+	printf("--before strlcpy--\n");
+	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, ori_src, dstsize);
+	printf("dst src overlap is abort\n");
+	// ori_ret = strlcpy(ori_dst, ori_src, dstsize);
+	// ft_ret = ft_strlcpy(ft_dst, ft_src, dstsize);
+	// printf("%zu\n, %s\n", ft_ret, ft_dst);
+	// if (ori_ret == ft_ret)
+	// {
+	// 	printf("--after strlcpy--\n");
+	// 	printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
+	// 	printf("OK :)\n");
+	// }
+	// else
+	// {
+	// 	printf("NG :(\n");
+	// 	exit (0);
+	// }
+	return ;
+}
+
+void	test11_dst_overlap_src_case2(void)
+{
+	char	ori_src[30] = "Hello, 42 world!\0";
+	// char	ft_src[30] = "Hello, 42 world!\0";
+	char	*ori_dst = &ori_src[7];
+	// char	*ft_dst = &ft_src[7];
+	size_t	dstsize = strlen(ori_src) * 2;
+	// size_t	ori_ret;
+	// size_t	ft_ret;
+
+	printf("\n///test11_dst_overlap_src_case2///\n");
+	printf("--before strlcpy--\n");
+	printf("dst: %s\nsrc: %s\ndstsize: %zu\n", ori_dst, ori_src, dstsize);
+	printf("dst src overlap is abort\n");
+	// ori_ret = strlcpy(ori_dst, ori_src, dstsize);
+	// ft_ret = ft_strlcpy(ft_dst, ft_src, dstsize);
+	// printf("%zu\n, %s\n", ft_ret, ft_dst);
+	// if (ori_ret == ft_ret)
+	// {
+	// 	printf("--after strlcpy--\n");
+	// 	printf("ret: %zu\ndst: %s\n", ori_ret, ori_dst);
 	// 	printf("OK :)\n");
 	// }
 	// else
