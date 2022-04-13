804ac23619de5838bdea09d97962eb5111bce6ce
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 12:35:18 +0900

    isprint_test

--- ft_isprint.c
+++ ft_isprint.c
@@ -23,23 +23,34 @@ int	ft_isprint(int c)
 
 int main(void)
 {
-	int	i = -256;
+	int	i = 31;
+	int	c = -10000;
 	int	ori = 0;
 	int	ft = 0;
-
 	printf("ascii:\tisprint\t:\tft_isprint\n");
-	while (i < 257)
+	while (i <= 127)
 	{
 		ori = isprint(i);
 		ft = ft_isprint(i);
 		if (ori == ft)
-			printf("[%d]:\t%d\t:\t%d\n", i, ori, ft);
+			printf("[%c]:\t%d\t:\t%d\n", i, ori, ft);
 		else
 		{
 			printf("NG :(\n");
 			return(0);
 		}
 		i++;
+	}
+	while (c <= 10000)
+	{
+		ori = isprint(c);
+		ft = ft_isprint(c);
+		if (ori != ft)
+		{
+			printf("NG :(\n");
+			return(0);
+		}
+		c++;
 	}
 	printf("OK :)\n");
 	return (0);
