a2ee986d52c47921ad908bc098c836e92f276e34
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 23:35:10 +0900

    lstclear?????????

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,22 +6,10 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:08:52 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/13 17:24:58 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/13 23:32:50 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	size_t	i;
-
-	i = 0;
-	while (lst[i] != NULL)
-	{
-		del(lst[i]);
-		free(lst[i]->next);
-		i++;
-	}
-	free(lst);
-}
+/* void	ft_lstclear(t_list **lst, void (*del)(void *)) */
