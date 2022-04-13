80f8cbd58d00da9b6df68110b3729a34b42614cb
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 17:25:21 +0900

    lstclear_f_void

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:08:52 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/13 17:12:17 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/13 17:24:58 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	i = 0;
 	while (lst[i] != NULL)
 	{
-		lst[i]->content = del(lst[i]);
-		free(lst[i].next);
+		del(lst[i]);
+		free(lst[i]->next);
 		i++;
 	}
 	free(lst);
