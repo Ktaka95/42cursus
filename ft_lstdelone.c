0a16739c89fd415ddf5e486e8d649bf93631833c
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 17:24:31 +0900

    lstdelone_f_void

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:04:08 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/13 17:08:21 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/13 17:23:56 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	lst->content = del(lst);
-	free(lst.content);
+	del(lst);
+	free(lst->content);
+	return ;
 }
