3163ab6b35d5948b9befdc7823facd92e340316f
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 23:22:58 +0900

    lstlast_modify

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 16:50:59 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/13 16:53:29 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/13 23:22:44 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst != NULL)
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
 }
