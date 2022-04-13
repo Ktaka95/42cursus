18f2e5cae4da2b701bfaafab3d6aba8794a1ca4a
Ktaka95 <ktakada950711@gmail.com>
2022-04-13 17:00:55 +0900

    lstadd_front_typo

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 16:38:10 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/13 16:58:22 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/13 16:59:49 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	i = 0;
 	while (lst[i] != NULL)
 		i++;
-	ist[i + 1] = NULL;
+	lst[i + 1] = NULL;
 	while (i > 0)
 	{
-		ist[i] ist[i - 1];
+		lst[i] = lst[i - 1];
 		i--;
 	}
-	ist[i] = new;
+	lst[i] = new;
 	return ;
 }
