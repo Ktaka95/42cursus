31064f63d53eca25cc5873d423cf4df58e2e1eb3
Ktaka95 <ktakada950711@gmail.com>
2022-04-14 20:58:14 +0900

    lstsize_test_comp

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 16:41:39 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/14 20:06:56 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/14 20:57:57 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,59 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	int	count;
+	long	count;
 
 	count = 0;
 	while (lst != NULL)
 	{
+		if (count == INT_MAX)
+			return (0);
 		count++;
 		lst = lst->next;
 	}
-	return (count);
+	return ((int) count);
 }
 
-int	main()
+/*
+#include <stdio.h>
+
+void	test1_normal_case(void);
+void	test2_lst_null_case(void);
+
+int	main(void)
+{
+	test1_normal_case();
+	test2_lst_null_case();
+	return (0);
+}
+
+
+void	test1_normal_case(void)
+{
+	t_list	*lst1 = ft_lstnew((void *)"Hello,");
+	t_list	*lst2 = ft_lstnew((void *)"42");
+	t_list	*lst3 = ft_lstnew((void *)"world!");
+	int		lstsize;
+
+	lst1->next = lst2;
+	lst2->next = lst3;
+	lst3->next = NULL;
+	t_list	*lst = lst1;
+
+	printf("///test1_normal_case///\n");
+	lstsize = ft_lstsize(lst);
+	printf("%d\n", lstsize);
+	return ;
+}
+
+void	test2_lst_null_case(void)
+{
+	t_list	*lst = NULL;
+	int		lstsize;
+
+	printf("\n///test2_lst_null_case///\n");
+	lstsize = ft_lstsize(lst);
+	printf("%d\n", lstsize);
+	return ;
+}
+*/
