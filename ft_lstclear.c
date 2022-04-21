dd72ca7b6d1ce4e69a7e461176343fd40938042e
Ktaka95 <ktakada950711@gmail.com>
2022-04-21 12:55:17 +0900

    lstclear_test_modify

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 13:37:20 by ktakada           #+#    #+#             */
-/*   Updated: 2022/04/19 22:38:05 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/21 12:55:04 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		ft_lstdelone(*lst, del);
 		*lst = next;
 	}
-	*lst = NULL;
 	return ;
 }
 
@@ -100,7 +99,9 @@ void	test2_normal_case2(void)
 	printf("\n--after lstclear--\n");
 	ft_lstclear(&lst2, free);
 	printf("lst[0]:\t%p\n", lst0);
+	printf("lst[0]-content:\t%s\n", (char *)lst0->content);
 	printf("lst[1]:\t%p\n", lst1);
+	printf("lst[1]-content:\t%s\n", (char *)lst1->content);
 	printf("lst[2]:\t%p\n", lst2);
 	return ;
 }
@@ -147,7 +148,6 @@ void	test3_lst_null_case(void)
 
 void	test4_del_null_case(void)
 {
-	t_list	**lst = NULL;
 	t_list	*start = NULL;
 	t_list	*lst0 = ft_lstnew(strdup("Hello,"));
 	t_list	*lst1 = ft_lstnew(strdup("42"));
@@ -160,7 +160,6 @@ void	test4_del_null_case(void)
 	start = lst0;
 	printf("\n///test4_del_null_case///\n");
 	printf("--before lstclear--\n");
-	printf("lst:\t%p\n", lst);
 	while (start != NULL)
 	{
 		printf("lst[%d]-now:\t%p\n", i, start);
@@ -170,18 +169,11 @@ void	test4_del_null_case(void)
 		i++;
 	}
 	printf("\n--after lstclear--\n");
-	ft_lstclear(lst, NULL);
+	ft_lstclear(&lst0, NULL);
 	i = 0;
-	start = lst0;
-	printf("lst:\t%p\n", lst);
-	while (start != NULL)
-	{
-		printf("lst[%d]-now:\t%p\n", i, start);
-		printf("lst[%d]-content:\t%s\n", i, (char *)start->content);
-		printf("lst[%d]-next:\t%p\n", i, start->next);
-		start = start->next;
-		i++;
-	}
+	printf("lst[0]:\t%p\n", lst0);
+	printf("lst[1]:\t%p\n", lst1);
+	printf("lst[2]:\t%p\n", lst2);
 	return ;
 }
 */
