af779d80cc40b50c47dc0181f974ef63e01295e5
Ktaka95 <ktakada950711@gmail.com>
2022-04-21 12:37:08 +0900

    strncmp_no_use_strlen

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktakada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 16:41:36 by ktakada           #+#    #+#             */
-/*   Updated: 2021/10/12 16:41:38 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/04/21 12:36:55 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(const char	*s1, const char	*s2, size_t	n)
 	i = 0;
 	str1 = (unsigned char *) s1;
 	str2 = (unsigned char *) s2;
-	while (i < n && (i <= ft_strlen(s1) || i <= ft_strlen(s2)))
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
@@ -38,26 +38,26 @@ int	ft_strncmp(const char	*s1, const char	*s2, size_t	n)
 void	test1_normal_match_case(void);
 void	test2_normal_wrong_case(void);
 void	test3_n_gt_str_case(void);
-void	test4_str_include_0_case(void);
-void	test5_str_include_0_case2(void);
+void	test4_same_str_include_0_case(void);
+void	test5_not_same_str_include_0_case(void);
 void	test6_n_0_case(void);
-void	test7_str_empty_case(void);
-void	test8_str_null_n_0_case(void);
-void	test9_both_str_null_case(void);
-void	test10_str_null_case(void);
+void	test7_one_str_empty_case(void);
+void	test8_both_str_null_n_0_case(void);
+void	test9_both_str_null_n_not_0_case(void);
+void	test10_one_str_null_n_not_0_case(void);
 
 int	main(void)
 {
 	test1_normal_match_case();
 	test2_normal_wrong_case();
 	test3_n_gt_str_case();
-	test4_str_include_0_case();
-	test5_str_include_0_case2();
+	test4_same_str_include_0_case();
+	test5_not_same_str_include_0_case();
 	test6_n_0_case();
-	test7_str_empty_case();
-	test8_str_null_n_0_case();
-	test9_both_str_null_case();
-	test10_str_null_case();
+	test7_one_str_empty_case();
+	test8_both_str_null_n_0_case();
+	test9_both_str_null_n_not_0_case();
+	test10_one_str_null_n_not_0_case();
 	return (0);
 }
 
@@ -124,13 +124,13 @@ void	test3_n_gt_str_case(void)
 	return ;
 }
 
-void	test4_str_include_0_case(void)
+void	test4_same_str_include_0_case(void)
 {
 	char	*s1 = "Hello, 42\0 world!";
 	char	*s2 = "Hello, 42\0 world!";
-	size_t	n = strlen(s1);
+	size_t	n = 20;
 
-	printf("\n///test4_str_include_0_case///\n");
+	printf("\n///test4_same_str_include_0_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
 	{
@@ -145,13 +145,13 @@ void	test4_str_include_0_case(void)
 	return ;
 }
 
-void	test5_str_include_0_case2(void)
+void	test5_not_same_str_include_0_case(void)
 {
 	char	*s1 = "Hello, 42\0 world!";
 	char	*s2 = "Hello, 42\0 world?";
-	size_t	n = strlen(s1);
+	size_t	n = 20;
 
-	printf("\n///test5_str_include_0_case2///\n");
+	printf("\n///test5_not_same_str_include_0_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
 	{
@@ -187,13 +187,13 @@ void	test6_n_0_case(void)
 	return ;
 }
 
-void	test7_str_empty_case(void)
+void	test7_one_str_empty_case(void)
 {
 	char	*s1 = "Hello, 42 world!";
 	char	*s2 = "";
 	size_t	n = strlen(s1);
 
-	printf("\n///test7_str_empty_case///\n");
+	printf("\n///test7_one_str_empty_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
 		printf("OK :)\n");
@@ -205,13 +205,13 @@ void	test7_str_empty_case(void)
 	return ;
 }
 
-void	test8_str_null_n_0_case(void)
+void	test8_both_str_null_n_0_case(void)
 {
 	char	*s1 = NULL;
 	char	*s2 = NULL;
 	size_t	n = 0;
 
-	printf("\n///test8_str_null_case///\n");
+	printf("\n///test8_both_str_null_n_0_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
 	{
@@ -226,13 +226,13 @@ void	test8_str_null_n_0_case(void)
 	return ;
 }
 
-void	test9_both_str_null_case(void)
+void	test9_both_str_null_n_not_0_case(void)
 {
 	char	*s1 = NULL;
 	char	*s2 = NULL;
 	size_t	n = 10;
 
-	printf("\n///test9_both_str_null_case///\n");
+	printf("\n///test9_both_str_null_n_not_0_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	printf("***This case is SEGV***\n");
 	// if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
@@ -245,13 +245,13 @@ void	test9_both_str_null_case(void)
 	return ;
 }
 
-void	test10_str_null_case(void)
+void	test10_one_str_null_n_not_0_case(void)
 {
 	char	*s1 = "Hello, 42 world!";
 	char	*s2 = NULL;
 	size_t	n = strlen(s1);
 
-	printf("\n///test10_str_null_case///\n");
+	printf("\n///test10_one_str_null_n_not_0_case///\n");
 	printf("s1: %s\ns2: %s\nn: %zu\n", s1, s2, n);
 	printf("***This case is SEGV***\n");
 	// if (strncmp(s1, s2, n) == ft_strncmp(s1, s2, n))
