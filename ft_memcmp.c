31f81f4b3d77aac4d12210190e59beed916f85d8
Ktaka95 <ktakada950711@gmail.com>
2022-04-22 21:42:44 +0900

    memcmp_test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ktaka <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 16:10:02 by ktaka             #+#    #+#             */
-/*   Updated: 2022/01/28 16:10:03 by ktaka            ###   ########.fr       */
+/*   Updated: 2022/04/22 21:42:30 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,221 +34,178 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 #include <string.h>
 #include <stdio.h>
 
-static void	memcmp_len_0_case1(char *s1, char *s2);
-static void	memcmp_len_0_case2(char *s1, char *s2);
-static void	memcmp_len_0_casenull(char *s1, char *s2);
-static void	memcmp_normal_case(char *s1, char *s2);
-static void	memcmp_diff_after_n_case(char *s1, char *s2);
-static void	including_null_case1(char *s1, char *s2);
-static void	including_null_case2(char *s1, char *s2);
+void	test1_len_0_case(void);
+void	test2_len_0_case2(void);
+void	test3_null_null_0_cmp_case(void);
+void	test4_normal_cmp_case(void);
+void	test5_diff_after_n_case(void);
+void	test6_including_null_case(void);
+void	test7_including_null_case2(void);
+void	test8_null_null_n_not_0_case(void);
 
 int main(void)
 {
-	char	len_0_case1[] = "";
-	char	len_0_case2[] = "abc";
-
-	char	*null1 = NULL;
-	char	*null2 = NULL;
-
-	char	normal_case[] = "abcda";
-	char	normal_case2[] = "abcd";
-
-	char	including_null1[] = "\0a\0b\0c1";
-	char	including_null2[] = "\0a\0b\0c1";
-
-	char	including_null3[] = "abc\0d\0e1";
-	char	including_null4[9] = "abc\0d";
-
-	memcmp_len_0_case1(len_0_case1, len_0_case2);
-	memcmp_len_0_case2(len_0_case2, len_0_case1);
-	memcmp_len_0_casenull(null1, null2);
-	memcmp_normal_case(normal_case, normal_case2);
-	memcmp_diff_after_n_case(normal_case, normal_case2);
-	including_null_case1(including_null1, including_null2);
-	including_null_case2(including_null3, including_null4);
-	printf("/////Below case results segmentation fault/////\n");
-	printf("len != 0 && (s1 == NULL || s2 == NULL)\n");
-	// ft_memcmp(null1, null2, 1);
-	// memcmp(null1, null2, 1);
+	test1_len_0_case();
+	test2_len_0_case2();
+	test3_null_null_0_cmp_case();
+	test4_normal_cmp_case();
+	test5_diff_after_n_case();
+	test6_including_null_case();
+	test7_including_null_case2();
+	test8_null_null_n_not_0_case();
 	return(0);
 }
 
-void	memcmp_len_0_case1(char *s1, char *s2)
+void	test1_len_0_case(void)
 {
+	char	s1[] = "";
+	char	s2[] = "abc";
 	int		ori_ret = 0;
 	int		ft_ret = 0;
 	int		i = 0;
 
-	printf("\n/////len_0_case1/////\n");
-	printf("\"\": %s → %d byte cmp\n\n", s2, i);
+	printf("///test1_len_0_case///\n");
+	printf("%s\"\": %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
 	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	memcmp_len_0_case2(char *s1, char *s2)
+void	test2_len_0_case2(void)
 {
+	char	s1[] = "abc";
+	char	s2[] = "";
 	int		ori_ret = 0;
 	int		ft_ret = 0;
 	int		i = 0;
 
-	printf("\n/////len_0_case2/////\n");
-	printf("%s: \"\" → %d byte cmp\n\n", s1, i);
+	printf("\n///test2_len_0_case2///\n");
+	printf("%s: %s\"\", %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
 	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	memcmp_len_0_casenull(char *s1, char *s2)
+void	test3_null_null_0_cmp_case(void)
 {
+	char	*s1 = NULL; 
+	char	*s2 = NULL;
 	int		ori_ret = 0;
 	int		ft_ret = 0;
 	int		i = 0;
 
-	printf("\n/////len_0_casenull/////\n");
-	printf("%s: %s → %d byte cmp\n\n", s1, s2, i);
+	printf("\n///test3_null_null_0_cmp_case///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
 	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	memcmp_normal_case(char *s1, char *s2)
+void	test4_normal_cmp_case(void)
 {
+	char	*s1 = "abcda";
+	char	*s2 = "abcd";
 	int		ori_ret = 0;
 	int		ft_ret = 0;
-	int		i = 5;
-	int		j = 0;
-	int		k = 0;
-	int		_ = 0;
+	int		i = strlen(s1);
 
-	printf("\n/////normal_case/////\n");
-	printf("%s: %s → %d byte cmp\n\n", s1, s2, i);
-	printf("Index:\t");
-	for (_ = 0; _ < i; _++)
-		printf("%d\t", _);
-	printf("\n%s:\t", s1);
-	while (j < i)
-	{
-		printf("[%d]\t", s1[j]);
-		j++;
-	}
-	printf("\n%s:\t", s2);
-	for (k = 0; k < i; k++)
-		printf("[%d]\t", s2[k]);
+	printf("\n///test4_normal_cmp_case///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
-	printf("\n\nori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
+	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	memcmp_diff_after_n_case(char *s1, char *s2)
+void	test5_diff_after_n_case(void)
 {
+	char	*s1 = "abcda";
+	char	*s2 = "abcd";
 	int		ori_ret = 0;
 	int		ft_ret = 0;
-	int		i = 4;
-	int		j = 0;
-	int		k = 0;
-	int		_ = 0;
+	int		i = strlen(s2);
 
-	printf("\n/////memcmp_diff_after_n_case/////\n");
-	printf("%s: %s → %d byte cmp\n\n", s1, s2, i);
-	printf("Index:\t");
-	for (_ = 0; _ < i; _++)
-		printf("%d\t", _);
-	printf("\n%s:\t", s1);
-	while (j < i)
-	{
-		printf("[%d]\t", s1[j]);
-		j++;
-	}
-	printf("\n%s:\t", s2);
-	for (k = 0; k < i; k++)
-		printf("[%d]\t", s2[k]);
+	printf("\n///test5_diff_after_n_case///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
-	printf("\n\nori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
+	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	including_null_case1(char *s1, char *s2)
+void	test6_including_null_case(void)
 {
+	char	*s1 = "\0a\0b\0c1";
+	char	*s2 = "\0a\0b\0c1";
+	int		ori_ret = 0;
+	int		ft_ret = 0;
+	int		i = 7;
+
+	printf("\n///test6_including_null_case///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
+	ft_ret = ft_memcmp(s1, s2, i);
+	ori_ret = memcmp(s1, s2, i);
+	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
+	if (ft_ret == ori_ret)
+		printf("OK :)\n");
+	else
+		printf("\nNG :(\n");
+}
+
+void	test7_including_null_case2(void)
+{
+	char	*s1 = "abc\0d\0e1";
+	char	*s2 = "abc\0d";
 	int		ori_ret = 0;
 	int		ft_ret = 0;
 	int		i = 8;
-	int		j = 0;
-	int		k = 0;
-	int		_ = 0;
 
-	printf("\n/////including_null_case1/////\n");
-	printf("\\0a\\0b\\0c1: \\0a\\0b\\0c1 → %d byte cmp\n\n", i);
-	printf("Index:\t\t");
-	for (_ = 0; _ < i; _++)
-		printf("%d\t", _);
-	printf("\n\\0a\\0b\\0c1:\t");
-	while (j < i)
-	{
-		printf("[%d]\t", s1[j]);
-		j++;
-	}
-	printf("\n\\0a\\0b\\0c1:\t");
-	for (k = 0; k < i; k++)
-		printf("[%d]\t", s2[k]);
+	printf("\n///test7_including_null_case2///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
-	printf("\n\nori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
+	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
 }
 
-void	including_null_case2(char *s1, char *s2)
+void	test8_null_null_n_not_0_case(void)
 {
+	char	*s1 = NULL;
+	char	*s2 = NULL;
 	int		ori_ret = 0;
 	int		ft_ret = 0;
-	int		i = 9;
-	int		j = 0;
-	int		k = 0;
-	int		_ = 0;
+	int		i = 1;
 
-	printf("\n/////including_null_case2/////\n");
-	printf("\\0a\\0b\\0c1: \\0a\\0b\\0c1 → %d byte cmp\n\n", i);
-	printf("Index:\t\t");
-	for (_ = 0; _ < i; _++)
-		printf("%d\t", _);
-	printf("\nabc\\0d\\0e1:\t");
-	while (j < i)
-	{
-		printf("[%d]\t", s1[j]);
-		j++;
-	}
-	printf("\nabc\\0d:\t\t");
-	for (k = 0; k < i; k++)
-		printf("[%d]\t", s2[k]);
+	printf("\n///test8_null_null_n_not_0_case///\n");
+	printf("%s: %s, %dbyte cmp\n", s1, s2, i);
 	ft_ret = ft_memcmp(s1, s2, i);
 	ori_ret = memcmp(s1, s2, i);
-	printf("\n\nori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
+	printf("ori_ret\tft_ret\n%d\t%d\n", ori_ret, ft_ret);
 	if (ft_ret == ori_ret)
-		printf("\nOK :)\n");
+		printf("OK :)\n");
 	else
 		printf("\nNG :(\n");
+
 }
 */
